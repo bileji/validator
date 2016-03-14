f3ffd72a50fe62f7278c9ebcbd199cdddd7687bc
shuchao <shuchao@medlinker.net>
2016-03-14 20:14:23 +0800

    validator

--- ValidatorHeader.php
+++ ValidatorHeader.php
@@ -49,23 +49,59 @@ abstract class ValidatorHeader
     protected $defaultMessagesTemplate = [
         'map' => [
             self::VALIDATOR_CODE_LABEL => -50001,
-            self::VALIDATOR_MESSAGE_LABEL => '字段:field的值:value为非字典'
+            self::VALIDATOR_MESSAGE_LABEL => '字段:field的值:value不为字典'
         ],
         'list' => [
             self::VALIDATOR_CODE_LABEL => -50002,
-            self::VALIDATOR_MESSAGE_LABEL => '字段:field的值:value为非列表'
+            self::VALIDATOR_MESSAGE_LABEL => '字段:field的值:value不为列表'
         ],
         'string' => [
             self::VALIDATOR_CODE_LABEL => -50003,
-            self::VALIDATOR_MESSAGE_LABEL => '字段:field的值:value为非字符串'
+            self::VALIDATOR_MESSAGE_LABEL => '字段:field的值:value不为字符串'
         ],
         'numeric' => [
             self::VALIDATOR_CODE_LABEL => -50004,
-            self::VALIDATOR_MESSAGE_LABEL => '字段:field的值:value为非整型'
+            self::VALIDATOR_MESSAGE_LABEL => '字段:field的值:value不为整型'
         ],
         'required' => [
             self::VALIDATOR_CODE_LABEL => -50005,
             self::VALIDATOR_MESSAGE_LABEL => '字段:field为必填字段'
+        ],
+        'between' => [
+            self::VALIDATOR_CODE_LABEL => -50006,
+            self::VALIDATOR_MESSAGE_LABEL => '字段:field的值:value必须介于:min-:max之间'
+        ],
+        'url' => [
+            self::VALIDATOR_CODE_LABEL => -50007,
+            self::VALIDATOR_MESSAGE_LABEL => '字段:field的值:value不是正确的url格式'
+        ],
+        'mail' => [
+            self::VALIDATOR_CODE_LABEL => -50008,
+            self::VALIDATOR_MESSAGE_LABEL => '字段:field的值:value不符合邮箱格式'
+        ],
+        'phone' => [
+            self::VALIDATOR_CODE_LABEL => -50009,
+            self::VALIDATOR_MESSAGE_LABEL => '字段:field的值:value不符合手机号格式'
+        ],
+        'chinese' => [
+            self::VALIDATOR_CODE_LABEL => -50009,
+            self::VALIDATOR_MESSAGE_LABEL => '字段:field的值:value不符合中文格式'
+        ],
+        'regex' => [
+            self::VALIDATOR_CODE_LABEL => -50010,
+            self::VALIDATOR_MESSAGE_LABEL => '字段:field的值:value不符合:regex格式'
+        ],
+        'before' => [
+            self::VALIDATOR_CODE_LABEL => -50011,
+            self::VALIDATOR_MESSAGE_LABEL => '字段:field的值:value不在:time之前'
+        ],
+        'after' => [
+            self::VALIDATOR_CODE_LABEL => -50012,
+            self::VALIDATOR_MESSAGE_LABEL => '字段:field的值:value不在:time之后'
+        ],
+        'enum' => [
+            self::VALIDATOR_CODE_LABEL => -50013,
+            self::VALIDATOR_MESSAGE_LABEL => '字段:field的值:value不在枚举的值:enum之中'
         ],
     ];
 
@@ -92,5 +128,55 @@ abstract class ValidatorHeader
     protected function validatorRequired($value)
     {
         return !empty($value) ? $value : self::VALIDATOR_BREAK_MD5;
+    }
+
+    protected function validatorBetween($value, $min, $max)
+    {
+        return $value >= $min && $value <= $max ? $value : null;
+    }
+
+    protected function validatorUrl($value)
+    {
+        return preg_match('/^https?:\/\/(([a-zA-Z0-9_-])+(\.)?)*(:\d+)?(\/((\.)?(\?)?=?&?[a-zA-Z0-9_-](\?)?)*)*$/i', $value) ? $value : null;
+    }
+
+    protected function validatorEmail($value)
+    {
+        return preg_match('/\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/', $value) ? $value : null;
+    }
+
+    protected function validatorPhone($value)
+    {
+        return preg_match('/^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/', $value) ? $value : null;
+    }
+
+    protected function validatorChinese($value)
+    {
+        return preg_match('/[\u4e00-\u9fa5]/gm', $value) ? $value : null;
+    }
+
+    protected function validatorRegex($value, $regex)
+    {
+        return preg_match($regex, $value) ? $value : null;
+    }
+
+    protected function validatorBefore($value, $time)
+    {
+        return ($value < !is_int($time) ? strtotime($time) : $time) ? $value : null;
+    }
+
+    protected function validatorAfter($value, $time)
+    {
+        return ($value < !is_int($time) ? strtotime($time) : $time) ? $value : null;
+    }
+
+    protected function validatorDefault($value, $default)
+    {
+        return empty($value) ? $default : $value;
+    }
+
+    protected function validatorEnum($value, $enum)
+    {
+        return in_array($value, explode('-', $enum)) ? $value : null;
     }
 }
